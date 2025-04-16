bef1a799f983805c9f9f0338d247bde9127c2437
Muhammad Arkan Raihan <markanraihan@gmail>
2025-04-16 20:32:47 +0700

    fix index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 require('dotenv').config()
+
+const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use('/', (req, res, next) => {
@@ -7,5 +9,5 @@ app.use('/', (req, res, next) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`server berhasil running di port ${PORT}`);
+    console.log(`server berhasil berjalan di port ${PORT}`);
 })
